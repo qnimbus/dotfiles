8312110d550823ea0206234966fde8b174ccd8a3
B. van Wetten <bas@vanwetten.com>
2025-10-14 21:28:12 +0200

    Update .chezmoiscripts/windows/remove-wsb.ps1

--- home/.chezmoiscripts/windows/run_onchange_remove-wsb.ps1
+++ home/.chezmoiscripts/windows/run_onchange_remove-wsb.ps1
@@ -1,2 +1,13 @@
+# --- Self-elevate ---
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+  ).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+  $argsLine = "-ExecutionPolicy Bypass -NoProfile -File `"$($MyInvocation.MyCommand.Path)`" " + $MyInvocation.UnboundArguments
+  Start-Process -Wait -FilePath PowerShell.exe -Verb RunAs -ArgumentList $argsLine
+  exit
+}
+
+# Tidy output (optional)
+$ProgressPreference = 'SilentlyContinue'
+
 New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force
 New-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "EnableDynamicContentInWSB" -PropertyType DWORD -Value 0
