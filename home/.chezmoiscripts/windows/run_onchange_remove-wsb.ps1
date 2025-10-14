376febcdbca0f854b182642498728b133f0e4599
B. van Wetten <bas@vanwetten.com>
2025-10-14 21:28:56 +0200

    Update .chezmoiscripts/windows/remove-wsb.ps1

--- home/.chezmoiscripts/windows/run_onchange_remove-wsb.ps1
+++ home/.chezmoiscripts/windows/run_onchange_remove-wsb.ps1
@@ -11,3 +11,19 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 
 New-Item "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force
 New-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "EnableDynamicContentInWSB" -PropertyType DWORD -Value 0
+
+# Wait 5 seconds of a keypress before closing
+Write-Host ""
+Write-Host "Done. (Press any key to close, or wait 5 seconds...)"
+$timeout = 5
+$start = Get-Date
+while ($true) {
+    if ($host.UI.RawUI.KeyAvailable) {
+        $null = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        break
+    }
+    if ((Get-Date) - $start -gt (New-TimeSpan -Seconds $timeout)) {
+        break
+    }
+    Start-Sleep -Milliseconds 100
+}
